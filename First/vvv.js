fb9ddda1765626aca3c026f8681c2d8a45b1a39b
HYT34 <124575355+HYT34@users.noreply.github.com>
2023-12-06 01:14:06 -0800

    Create vvv.js

---
+++ First/vvv.js
@@ -0,0 +1,7 @@
+// "user strict";
+// const ps=require("prompt-sync");
+// const prompt=ps();
+const mass=+prompt("what is ur mass");
+const h=+prompt("what is ur mass");
+const BMI=mass/(h*h);
+console.log(BMI);
